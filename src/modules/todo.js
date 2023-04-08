6a7e3601f64def2f0a0e573b2362db5867e33a73
mjnchy <mijanchowdhury2001@gmail.com>
2023-04-08 14:25:18 -0400

    Added some logic and ui

---
+++ src/modules/todo.js
@@ -0,0 +1,44 @@
+const todoList = {};
+const projects = {};
+
+function createTodo (title, description, dueDate, _projects, priority) {
+    todoList[title] = {
+        title, description, dueDate, _projects
+    };
+    return todoList;
+};
+
+createTodo('title', 'description', 'dueDate', ['favourites', 'today']);
+
+function listProjects () {
+    const allProjects = [];
+    Object.keys(todoList).map(key => {
+        todoList[key]._projects.map(_project => {
+            allProjects.push(_project);
+        });
+    });
+
+    return allProjects;
+};
+
+function reduceProjects () {
+    const filteredProjects = [...new Set(listProjects())];
+    filteredProjects.forEach(project => {
+        projects[project] = {};
+    });
+
+    return filteredProjects;
+};
+
+function addToProjects () {
+    reduceProjects();
+    Object.keys(todoList).map(key => {
+        todoList[key]._projects.map(_project => {
+            projects[_project][key] = todoList[key];
+        });
+    });
+
+    return projects;
+}
+
+console.log(addToProjects());
