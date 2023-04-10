ed2d5838939baea624ab128b17770f51f2a68740
mjnchy <mijanchowdhury2001@gmail.com>
2023-04-10 17:39:17 -0400

    Minor Improvements

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -4,7 +4,7 @@ import "./DOMStuff.js";
 
 window.addEventListener('click', (e) => {
     if (e.target.id === 'add-btn' || e.target.classList.contains('fa-plus')) {
-        const formContainer = document.querySelector('.form-container');
-        formContainer.id = formContainer.id === 'form-container-hidden'? 'form-container-visible': 'form-container-hidden';
+        const formContainer = document.querySelector('#form-container');
+        formContainer.dataset.status = formContainer.dataset.status === 'inactive'? 'active': 'inactive';
     }
 })
